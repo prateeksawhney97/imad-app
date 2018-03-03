828186d9e77c137c95f5d67027a3551e08863282
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-03-03 21:30:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,62 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One | Prateek Sawhney',
+    heading : 'Article One',
+    date: 'March 02, 2018',
+    content:  `<p>
+                Lorem ipsum dolor sit amet, dolor a dolor orci feugiat eu, duis praesent viverra, fusce vestibulum lorem. Vitae nonummyluctus turpis, dictum vel, commodo platea nunc amet, eget vefames pellentesque. Bibendum commodo eros pulvinar sed, rhoncus scelerisque malesuada. Pulvinar sed sed mollis dolor eros leo.
+            </p>
+            <p>
+               Ipsum enim vestibulum torquent. Et ipsum mauris praesent, curabitur sed malesuada semper vestibulum et nunc. Sed sodales eros blandit vestibulum, sollicitudin ac arcu tempor a enim, ac et, placerat fusce luctus. Posuere eu a proin a, et dapibus fusce bibendum id, tortor tempus blandit wisi urna, lc vulputate. 
+            </p>
+            <p>
+               Et consequat cras, at dapibus in lectus, fringilla scelerisque necipit sed aliquam eros interdum odio auctor, suscipit et. Mauris neque ut, in sed ultricies, elit veniam praesent rhoncuornare in, ullamcorper sem ut et, lacus at gravida. Optio mattis maecenas, ad pellentesque pulvinar eu ut phasellus lectus. Orci mi commodo volutpat wisi, nostra curabitur tincidunt mi eleifend, eu quam enim feugiat. 
+            </p>`
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+var htmlTemplate =`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr>
+        <h3>
+            ${heading}
+        </h3>
+            <div>
+            ${date}
+            </div>
+        <div>
+        ${content}
+        </div>
+        </div>
+        </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
+    
 });
 
 app.get('/article-two', function(req, res){
