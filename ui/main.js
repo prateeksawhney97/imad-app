469a34df2cf3ff436ba9408ebd6a40b91e0550a7
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-03-04 13:45:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,25 @@
 // counter javascript code
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function(){
-  //Make a request to the counter endpoint.
+    
+  //Create a request object.
+  var request = new XMLHttpRequest();
+  
   //Capture a response and store it in a variable.
-  //Render the variable in the correct span.
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  request.onreadystatechange = function (){
+    if(request.readyState == XMLHttpRequest.DONE){
+        //Take some action
+        if(request.status == 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+
+            
+        }
+    }  
+    //Not Done yet.
+  };
+  //Make a request.
+  request.open('GET', 'http://prateeksawhney97.imad.hasura-app.io/counter', true);
+  request.send(null);
 };    
