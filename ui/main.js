b7c30d4bfa69db4cba5feebb6cc6893803ef8017
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-03-05 07:20:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,6 @@ button.onclick = function(){
 
 //Submit a name.
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
      
@@ -51,6 +49,8 @@ submit.onclick = function () {
 }  
 };
   //Make a request.
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open('GET', 'http://prateeksawhney97.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
   
