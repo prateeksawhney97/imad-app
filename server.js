d316927f03664c93ceba7df1fb881d5184907377
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-03-06 14:44:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,8 +122,8 @@ app.get('/articles/:articleName', function(req, res){
     // articles[articleName] = {} content object for article one
    //var articleName = req.params.articleName;
    
-   //Select * from article where title = ''; DELETE where a = 'asdf'
-   pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result){
+   //Select * from article where title = '\'; DELETE where a = \'asdf'
+   pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err, result){
        if (err) {
            res.status(500).send(err.toString());
        } else {
