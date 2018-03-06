e6fc460ee9e9d45c297f77345ccb6ab79235927c
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-03-06 11:49:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-
-//var config = {
-  //  user : 'prateeksawhney97',
-    //database : 'prateeksawhney97',
-//    host : 'db.imad.hasura-app.io',
-  //  port : '5432',
-    //password : process.env.DB_PASSWORD
-//};
+var config = {
+user: 'prateeksawhney97',
+database: 'prateeksawhney97',
+host: 'db.imad.hasura-app.io',
+port: '5432',
+password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
+
 
 var articles = {
 'article-one': {
@@ -86,18 +86,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-//var pool = newPool(config);
-//app.get('/test-db', function(req, res){
+var pool = new Pool(config);
+app.get('/test-db', function(req, res){
     //Make a select request.
-  //  pool.query('SELECT * FROM test', function(err, result) {
-    //    if (err) {
-      //      res.status(500).send(err.toString());
-        //} else {
-          //  res.send(JSON.stringify(result));
-        //}
-    //});
+        pool.query('SELECT * FROM test', function(err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
     //Return a response with the results.
-//});
+});
 
 var counter = 0;
 app.get('/counter', function(req, res){
