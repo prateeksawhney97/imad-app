7f1d441385995f528974ec2e6d7153dc83c982fe
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-03-06 14:04:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/submit-name', function(req, res) { //submit-name?name=xxxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('articles/:articleName', function(req, res){
+app.get('/articles/:articleName', function(req, res){
     // articleName == article-one
     // articles[articleName] = {} content object for article one
    //var articleName = req.params.articleName;
