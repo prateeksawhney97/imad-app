fef3910da914c33b1df71b9829e4404531e92ecb
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-03-29 13:51:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ function hash (input, salt){
 
 app.get('/hash/:input', function(req, res){
    var hashedString = hash(req.params.input, 'this-is-some-random-string');
-   res.send('hashedString');
+   res.send(hashedString);
 });
 
 
