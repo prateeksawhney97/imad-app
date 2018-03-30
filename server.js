af893e132bdd74dba13dbd90b388d1f86ad65129
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-03-30 22:22:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,29 @@ app.post('/create-user', function(req, res){
     });
 });
 
+app.post('/login', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('SELECT * FROM "user" username = $1', [username], function(err, result){
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if(result.rows.length === 0){
+                res.send(403).send('Username/Password is invalid');
+            } else {
+            // Need to Match the password.    
+            var dbString = result.rows[0].password;
+            var salt = dbString.split('$')[2];
+            var hashedPassword = hash(password, salt); //Creating a hash based on the password submitted and the original salt.
+            if(hashedPassword === dbString){
+            res.send('Credentials are correct !');
+                } else {
+            res.send(403).send('Username/Password is invalid');
+                }
+            }
+        }
+    });
+});
 
 var pool = new Pool(config);
 
