33023fe4929df5f3c354c547bf3497930dcd48fe
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-03-31 22:20:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ app.get('/check-login', function(req, res){
 
 app.get('/logout', function(req, res){
    delete req.session.auth;
-   res.send('Logged out Successfully !');
+   res.send('Logged  out Successfully !');
 });
 
 var pool = new Pool(config);
